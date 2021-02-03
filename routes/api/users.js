f1e10a39c3b2383fe18271c226c1051db3cdcc6e
oldUath <haiyangdong666@gmail.com>
2021-02-03 16:22:21 +0800

    检查邮箱注册新用户，添加到数据库

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,15 +1,36 @@
 // 登录和注册
 const express = require("express");
 const router = express.Router();
+const User = require("../../models/User");
 
 // GET api/users/test
 router.get("/test",(req,res)=>{
-    res.json({msg:"login works"})
+    res.json({msg:"login works"}) 
 })
 
 //POST api/users/register
 router.post("/register",(req,res)=>{
-    console.log(req.body);
+    // console.log(req.body);
+    // 查询数据库中是否有邮箱，并且注册
+    User.findOne({email:req.body.email})
+        .then((user)=>{
+            //如果返回一个user说明已经注册过了
+            if(user){
+                return res.status(400).json({email:"邮箱已经注册"})
+            }else{
+                //如果没有就创建一个新用户
+                const newUser = new User({
+                    name:req.body.name,
+                    email:req.body.email,
+                    avater,
+                    password:req.body.password,
+                })
+
+
+
+            }
+        })
+
 })
 
 module.exports = router;
